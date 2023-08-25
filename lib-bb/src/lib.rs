325bdf8fb797b3f9e3b26881a22bfbb90d34742a
benjamin paul <bpaul@bpaul.xyz>
2023-08-25 19:46:45 +1000

    Guess the data structures that are used by blackboard

--- lib-bb/src/lib.rs
+++ lib-bb/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// process:
+//
+// Authenticate with blackboard
+// Get list of courses
+// When in a course
+//  Get side bar
+//      Get items under sidebar
+
+#![allow(dead_code)]
+
+pub struct Course {
+    display_id: String,
+    display_name: String,
+    // The weird id that looks something like "_172757_1"
+    id: String,
+    tabs: Vec<TabEntry>,
+    // other stuff
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Sidebar entry
+enum TabEntry {
+    Link(String),
+    Directory(Vec<FileEntry>),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+// Content under a directory
+// i have no idea how these data structures are formatted
+// blackboard is such a mess
+enum FileEntry {
+    Directory(Vec<FileEntry>),
+    File {
+        title: String,
+        link: String,
+        attachments: Vec<()>,
+        content_id: String,
+        course_id: String,
+        // https://learn.uq.edu.au/webapps/blackboard/execute/content/file?cmd=view&content_id={content_id}&course_id={course_id}
+        // is the url of the file
+    },
 }
